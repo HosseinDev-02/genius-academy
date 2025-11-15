07f112802a334c396d8067e16c4c6f12a3b1b5fe
HosseinDev-02 <rostamidev2002@gmail.com>
2025-11-15 18:31:44 +0330

    add offers page

---
+++ src/app/admin-panel/offers/add/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function AddOfferPage() {
+  return (
+    <div>AddOfferPage</div>
+  )
+}
---
+++ src/app/admin-panel/offers/page.tsx
@@ -0,0 +1,25 @@
+import PageHeader from '@/src/components/admin-panel/PageHeader'
+import MenuItemsTable from '@/src/components/admin-panel/menus/MenusTable'
+import { Plus } from 'lucide-react'
+import Link from 'next/link'
+import React from 'react'
+
+export default function OffersPage() {
+  return (
+    <div dir="rtl">
+    {/* Page Header */}
+    <PageHeader title="منو ها" className="!flex" />
+    {/* Page Content */}
+    <div className="h-full overflow-hidden">
+        <Link
+            href="menus/add"
+            className="col-span-1 inline-flex items-center justify-center gap-2 text-sm h-10 px-3 rounded transition-colors duration-300 bg-teal-800 hover:bg-teal-600 font-YekanBakh-SemiBold text-white cursor-pointer mb-3"
+        >
+            <span>افزودن منو</span>
+            <Plus strokeWidth={"2.5px"} size={16} />
+        </Link>
+        {/* <MenuItemsTable data={menus} /> */}
+    </div>
+</div>
+  )
+}
